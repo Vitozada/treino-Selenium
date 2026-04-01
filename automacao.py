b86c67889b7f03b916815d7053b15d51515d5ecd
Vitor Gabriel Melo Silva <vitorextre@gmail.com>
2026-04-01 12:30:06 -0300

    clickar na setinha

--- automacao.py
+++ automacao.py
@@ -2,10 +2,14 @@ from selenium import webdriver;
 import time;
 
 navegador = webdriver.Chrome();
-navegador.maximize_window();
 navegador.get("https://www.pichau.com.br/");
+navegador.maximize_window();
 
 botao_departamento = navegador.find_elements("class name","MuiButtonBase-root")
+time.sleep(5)
 botao_departamento[4].click() ## clicou em departamentos
+
+barra = navegador.find_elements("class name", "mui-tjklty-arrow")
+barra[0].click()
 
 time.sleep(10)
