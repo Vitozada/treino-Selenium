453691aeb6fafb391079f75c980a69dde33c654d
Vitor Gabriel Melo Silva <vitorextre@gmail.com>
2026-04-02 11:03:24 -0300

    preenchendo

--- automacao.py
+++ automacao.py
@@ -8,10 +8,17 @@ navegador.maximize_window()
 titulo = navegador.title; # exemplo de como pegar o titulo da guia
 print(titulo)
 
+# primeiro input
 text_input = navegador.find_element("id","my-text-id");
 text_input.send_keys("inserindo o primeiro texto")
 
+# input de senha
 password_input = navegador.find_elements("class name", "form-control") # com isso consigo um array de todos os elementos da classe 
 password_input[1].send_keys("senha123") 
+
+# preenchendo o text area
+areaTexto = navegador.find_element("name","my-textarea");
+areaTexto.send_keys("Um texto sendo colocado no textArea da pagina de treinar preenchimento de formulario entregue na documentação do Selenium!!")
+
 
 time.sleep(5)
